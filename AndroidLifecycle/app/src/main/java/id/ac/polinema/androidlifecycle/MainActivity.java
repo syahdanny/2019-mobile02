3a961921922fa1b1f2fe5ea34fa8c19255c8d96b
syahdanny <syahdanny11@gmail.com>
2019-09-09 22:32:22 +0700

    menyempurnakan app androidlifecycle

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -3,15 +3,28 @@ package id.ac.polinema.androidlifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+	private EditText nameInput;
+	private TextView outputText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		nameInput = findViewById(R.id.input_name);
+		outputText = findViewById(R.id.text_output);
+
 	}
 
+	public void handleSubmit(View view) {
+		String name = nameInput.getText().toString();
+		outputText.setText("Hallo "+name);
+	}
 	// TODO: tambahkan callback onStart() di sini
 
 
